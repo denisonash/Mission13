f68175769f05b49a46c8fca0335806d2ae120a8a
ashleydenison <dashley9@byu.edu>
2022-03-30 08:45:38 -0600

    Bare requirements met

---
+++ Mission13/Mission13/Components/TeamViewComponent.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Mission13.Models;
+
+namespace Mission13.Components
+{
+    public class TeamViewComponent :ViewComponent
+    {
+        private IBowlersRepository _repo { get; set; }
+
+        public TeamViewComponent(IBowlersRepository temp)
+        {
+            _repo = temp;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.SelectedTeam = RouteData?.Values["teams"];
+
+            var teams = _repo.Teams.Select(t => t.TeamName)
+                .Distinct()
+                .OrderBy(t => t);
+
+            return View(teams);
+        }
+    }
+}
